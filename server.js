f20da1213f0d94e9641aa183a09ec9d8301a1e06
phillypak <abbasiyousaf99@gmail.com>
2021-11-25 23:50:12 -0500

    fixed all response statements

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("/app/", (req, res, next) => {
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
 app.post("/app/new/", (req, res) => {	
 	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)").run(req.body.user, md5(req.body.pass));
-	res.json({"message": '${stmt["changes"]} record created: ID ${stmt["lastInsertRowid"]} (201)'});
+	res.json({"message": `${stmt["changes"]} record created: ID ${stmt["lastInsertRowid"]} (201)`});
 	res.status(200);
 });
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
@@ -44,12 +44,14 @@ app.get("/app/user/:id", (req, res) => {
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {	
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?, user), pass = COALESCE(?, pass) WHERE id = ?").run(req.body.user, md5(req.body.pass), req.params.id);
-	res.status(200).json({"message":"1 record updated: ID 2 (200)"});
+	res.json({"message":`${stmt["changes"]} record updated: ID ${stmt["req.params.id"]} (200)`});
+	res.status(200);
 });
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete("/app/delete/user/:id", (req, res) => {	
 	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?").run(req.params.id);
-	res.status(200).json({"message":"1 record deleted: ID 2 (200)"});
+	res.json({"message":`${stmt["changes"]} record deleted: ID ${stmt["req.params.id"]} (200)`});
+	res.status(200);
 });
 // Default response for any other request
 app.use(function(req, res){
